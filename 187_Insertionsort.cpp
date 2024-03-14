8b89dfaefbd3373a44b491e2b48ac1b5e5c22687
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2024-03-14 11:32:51 +0700

    membuat display

--- 187_Insertionsort.cpp
+++ 187_Insertionsort.cpp
@@ -55,6 +55,18 @@ void insertionsort() {
 
 }
 
+void display() {
+	cout << endl;
+	cout << "=================================" << endl;
+	cout << "element array yang telah di susun" << endl;
+	cout << "=================================" << endl;
+
+	for (int j = 0; j < n; j++) {
+		cout << arr[j] << endl;
+	}
+	cout << endl;
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
