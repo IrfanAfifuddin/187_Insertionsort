eb3e767180407b37cee925cc0dc093c9c0798601
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2024-03-14 11:29:39 +0700

    membuat array dengan panjang data 20 dan membuat variable n

--- 187_Insertionsort.cpp
+++ 187_Insertionsort.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 using namespace std;
 
+int arr[20];                                               //membuat arrat dengan panjang data 20
+int n;                                                     //membuat variable imputan n
 
 int main()
 {
